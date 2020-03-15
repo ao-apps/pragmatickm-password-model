7034c8639a955d63de228ec262cecb57741b80cb
AO Industries, Inc <support@aoindustries.com>
2020-03-15 13:07:05 -0500

    com.aoindustries.util.StringUtility → com.aoindustries.lang.Strings

--- src/main/java/com/pragmatickm/password/model/Password.java
+++ src/main/java/com/pragmatickm/password/model/Password.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-password-model - Passwords nested within SemanticCMS pages and elements.
- * Copyright (C) 2013, 2014, 2015, 2016, 2019  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -26,7 +26,7 @@ import com.aoindustries.lang.NullArgumentException;
 import com.aoindustries.net.URIDecoder;
 import com.aoindustries.net.URIEncoder;
 import com.aoindustries.util.AoCollections;
-import static com.aoindustries.util.StringUtility.nullIfEmpty;
+import static com.aoindustries.lang.Strings.nullIfEmpty;
 import com.semanticcms.core.model.Element;
 import com.semanticcms.core.model.PageRef;
 import java.util.Collections;
