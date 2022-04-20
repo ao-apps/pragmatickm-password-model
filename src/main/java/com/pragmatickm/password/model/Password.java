8832bb924f0d3cf20e506e7d5e6b6ff0f53589fc
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:59:13 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/com/pragmatickm/password/model/Password.java
+++ src/main/java/com/pragmatickm/password/model/Password.java
@@ -37,167 +37,199 @@ import java.util.Objects;
 
 public class Password extends Element {
 
-	public static class CustomField {
+  public static class CustomField {
 
-		private final PageRef pageRef;
-		private final String element;
-		private final String value;
+    private final PageRef pageRef;
+    private final String element;
+    private final String value;
 
-		public CustomField(PageRef pageRef, String element, String value) {
-			value = nullIfEmpty(value);
-			if(pageRef == null && value == null) throw new IllegalArgumentException("At least one of page or value must be provided.");
-			this.pageRef = pageRef;
-			this.element = nullIfEmpty(element);
-			this.value = value;
-		}
+    public CustomField(PageRef pageRef, String element, String value) {
+      value = nullIfEmpty(value);
+      if (pageRef == null && value == null) {
+        throw new IllegalArgumentException("At least one of page or value must be provided.");
+      }
+      this.pageRef = pageRef;
+      this.element = nullIfEmpty(element);
+      this.value = value;
+    }
 
-		@Override
-		public String toString() {
-			if(value != null) return value;
-			String pageToString = pageRef.toString();
-			if(element == null) return pageToString;
-			// TODO: encodeIRIComponent to do this in one shot?
-			String elementIri = URIDecoder.decodeURI(URIEncoder.encodeURIComponent(element));
-			int sbLen =
-				pageToString.length()
-				+ 1 // '#'
-				+ elementIri.length();
-			StringBuilder sb = new StringBuilder(sbLen);
-			sb.append(pageToString).append('#').append(elementIri);
-			assert sb.length() == sbLen;
-			return sb.toString();
-		}
+    @Override
+    public String toString() {
+      if (value != null) {
+        return value;
+      }
+      String pageToString = pageRef.toString();
+      if (element == null) {
+        return pageToString;
+      }
+      // TODO: encodeIRIComponent to do this in one shot?
+      String elementIri = URIDecoder.decodeURI(URIEncoder.encodeURIComponent(element));
+      int sbLen =
+        pageToString.length()
+        + 1 // '#'
+        + elementIri.length();
+      StringBuilder sb = new StringBuilder(sbLen);
+      sb.append(pageToString).append('#').append(elementIri);
+      assert sb.length() == sbLen;
+      return sb.toString();
+    }
 
-		@Override
-		public boolean equals(Object o) {
-			if(!(o instanceof CustomField)) return false;
-			CustomField other = (CustomField)o;
-			return
-				Objects.equals(pageRef, other.pageRef)
-				&& Objects.equals(element, other.element)
-				&& Objects.equals(value, other.value)
-			;
-		}
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof CustomField)) {
+        return false;
+      }
+      CustomField other = (CustomField)o;
+      return
+        Objects.equals(pageRef, other.pageRef)
+        && Objects.equals(element, other.element)
+        && Objects.equals(value, other.value)
+      ;
+    }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(
-				pageRef,
-				element,
-				value
-			);
-		}
+    @Override
+    public int hashCode() {
+      return Objects.hash(
+        pageRef,
+        element,
+        value
+      );
+    }
 
-		public PageRef getPageRef() {
-			return pageRef;
-		}
+    public PageRef getPageRef() {
+      return pageRef;
+    }
 
-		public String getElement() {
-			return element;
-		}
+    public String getElement() {
+      return element;
+    }
 
-		public String getValue() {
-			return value;
-		}
-	}
+    public String getValue() {
+      return value;
+    }
+  }
 
-	private volatile String href;
-	private volatile String username;
-	private volatile String password;
-	private Map<String, CustomField> customFields;
-	private Map<String, String> secretQuestions;
+  private volatile String href;
+  private volatile String username;
+  private volatile String password;
+  private Map<String, CustomField> customFields;
+  private Map<String, String> secretQuestions;
 
-	@Override
-	public Password freeze() {
-		synchronized(lock) {
-			if(!frozen) {
-				customFields = AoCollections.optimalUnmodifiableMap(customFields);
-				secretQuestions = AoCollections.optimalUnmodifiableMap(secretQuestions);
-				super.freeze();
-			}
-		}
-		return this;
-	}
+  @Override
+  public Password freeze() {
+    synchronized (lock) {
+      if (!frozen) {
+        customFields = AoCollections.optimalUnmodifiableMap(customFields);
+        secretQuestions = AoCollections.optimalUnmodifiableMap(secretQuestions);
+        super.freeze();
+      }
+    }
+    return this;
+  }
 
-	public String getHref() {
-		return href;
-	}
+  public String getHref() {
+    return href;
+  }
 
-	public void setHref(String href) {
-		checkNotFrozen();
-		this.href = nullIfEmpty(href);
-	}
+  public void setHref(String href) {
+    checkNotFrozen();
+    this.href = nullIfEmpty(href);
+  }
 
-	public String getUsername() {
-		return username;
-	}
+  public String getUsername() {
+    return username;
+  }
 
-	public void setUsername(String username) {
-		checkNotFrozen();
-		this.username = nullIfEmpty(username);
-	}
+  public void setUsername(String username) {
+    checkNotFrozen();
+    this.username = nullIfEmpty(username);
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public void setPassword(String password) {
-		checkNotFrozen();
-		this.password = nullIfEmpty(password);
-	}
+  public void setPassword(String password) {
+    checkNotFrozen();
+    this.password = nullIfEmpty(password);
+  }
 
-	@SuppressWarnings("ReturnOfCollectionOrArrayField") // Returning unmodifiable
-	public Map<String, CustomField> getCustomFields() {
-		synchronized(lock) {
-			if(customFields == null) return Collections.emptyMap();
-			if(frozen) return customFields;
-			return AoCollections.unmodifiableCopyMap(customFields);
-		}
-	}
+  @SuppressWarnings("ReturnOfCollectionOrArrayField") // Returning unmodifiable
+  public Map<String, CustomField> getCustomFields() {
+    synchronized (lock) {
+      if (customFields == null) {
+        return Collections.emptyMap();
+      }
+      if (frozen) {
+        return customFields;
+      }
+      return AoCollections.unmodifiableCopyMap(customFields);
+    }
+  }
 
-	public void addCustomField(String name, PageRef pageRef, String element, String value) {
-		name = nullIfEmpty(name);
-		NullArgumentException.checkNotNull(name, "name");
-		synchronized(lock) {
-			checkNotFrozen();
-			if(customFields == null) customFields = new LinkedHashMap<>();
-			if(customFields.put(name, new CustomField(pageRef, element, value)) != null) throw new IllegalStateException("Duplicate custom field: " + name);
-			if(pageRef != null) addPageLink(pageRef);
-		}
-	}
+  public void addCustomField(String name, PageRef pageRef, String element, String value) {
+    name = nullIfEmpty(name);
+    NullArgumentException.checkNotNull(name, "name");
+    synchronized (lock) {
+      checkNotFrozen();
+      if (customFields == null) {
+        customFields = new LinkedHashMap<>();
+      }
+      if (customFields.put(name, new CustomField(pageRef, element, value)) != null) {
+        throw new IllegalStateException("Duplicate custom field: " + name);
+      }
+      if (pageRef != null) {
+        addPageLink(pageRef);
+      }
+    }
+  }
 
-	@SuppressWarnings("ReturnOfCollectionOrArrayField") // Returning unmodifiable
-	public Map<String, String> getSecretQuestions() {
-		synchronized(lock) {
-			if(secretQuestions == null) return Collections.emptyMap();
-			if(frozen) return secretQuestions;
-			return AoCollections.unmodifiableCopyMap(secretQuestions);
-		}
-	}
+  @SuppressWarnings("ReturnOfCollectionOrArrayField") // Returning unmodifiable
+  public Map<String, String> getSecretQuestions() {
+    synchronized (lock) {
+      if (secretQuestions == null) {
+        return Collections.emptyMap();
+      }
+      if (frozen) {
+        return secretQuestions;
+      }
+      return AoCollections.unmodifiableCopyMap(secretQuestions);
+    }
+  }
 
-	public void addSecretQuestion(String question, String answer) {
-		question = nullIfEmpty(question);
-		answer = nullIfEmpty(answer);
-		NullArgumentException.checkNotNull(question, "question");
-		NullArgumentException.checkNotNull(answer, "answer");
-		synchronized(lock) {
-			checkNotFrozen();
-			if(secretQuestions == null) secretQuestions = new LinkedHashMap<>();
-			if(secretQuestions.put(question, answer) != null) throw new IllegalStateException("Duplicate secret question: " + question);
-		}
-	}
+  public void addSecretQuestion(String question, String answer) {
+    question = nullIfEmpty(question);
+    answer = nullIfEmpty(answer);
+    NullArgumentException.checkNotNull(question, "question");
+    NullArgumentException.checkNotNull(answer, "answer");
+    synchronized (lock) {
+      checkNotFrozen();
+      if (secretQuestions == null) {
+        secretQuestions = new LinkedHashMap<>();
+      }
+      if (secretQuestions.put(question, answer) != null) {
+        throw new IllegalStateException("Duplicate secret question: " + question);
+      }
+    }
+  }
 
-	@Override
-	public String getLabel() {
-		//if(username != null) return username;
-		//if(href != null) return href;
-		String p = password;
-		if(p != null) return p;
-		return "Password";
-	}
+  @Override
+  public String getLabel() {
+    //if (username != null) {
+    //  return username;
+    //}
+    //if (href != null) {
+    //  return href;
+    //}
+    String p = password;
+    if (p != null) {
+      return p;
+    }
+    return "Password";
+  }
 
-	@Override
-	protected String getDefaultIdPrefix() {
-		return "password";
-	}
+  @Override
+  protected String getDefaultIdPrefix() {
+    return "password";
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-password-model - Passwords nested within SemanticCMS pages and elements.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,10 +21,10 @@
  * along with pragmatickm-password-model.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.pragmatickm.password.model {
-	exports com.pragmatickm.password.model;
-	// Direct
-	requires com.aoapps.collections; // <groupId>com.aoapps</groupId><artifactId>ao-collections</artifactId>
-	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
-	requires com.aoapps.net.types; // <groupId>com.aoapps</groupId><artifactId>ao-net-types</artifactId>
-	requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
+  exports com.pragmatickm.password.model;
+  // Direct
+  requires com.aoapps.collections; // <groupId>com.aoapps</groupId><artifactId>ao-collections</artifactId>
+  requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+  requires com.aoapps.net.types; // <groupId>com.aoapps</groupId><artifactId>ao-net-types</artifactId>
+  requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
 }
